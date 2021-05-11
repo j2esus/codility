da685878cac1d0c4d80355c5ebc8a33cedc45c1c
Jesús Vargas <jesus.vargas@jeegox.com>
2021-05-11 12:40:28 -0500

    Simplifying the second version of frog jumps

--- src/main/java/com/j2esus/lesson3/FrogJmpV1.java
+++ src/main/java/com/j2esus/lesson3/FrogJmpV1.java
@@ -2,6 +2,13 @@ package com.j2esus.lesson3;
 
 public class FrogJmpV1 {
     
+    /**
+     * this method gets 100% in codility
+     * @param X
+     * @param Y
+     * @param D
+     * @return 
+     */
     public static int calculateJumps(int X, int Y, int D) {
         int exp = (Y - X) / D, sum = 0;
         do{
--- src/main/java/com/j2esus/lesson3/FrogJmpV2.java
+++ src/main/java/com/j2esus/lesson3/FrogJmpV2.java
@@ -1,7 +1,15 @@
 package com.j2esus.lesson3;
 
 public class FrogJmpV2 {
+    
+    /**
+     * this method gets 100% in codility
+     * @param X
+     * @param Y
+     * @param D
+     * @return 
+     */
     public static int calculateJumps(int X, int Y, int D) {        
-        return ((X + D * ((Y - X) / D)) < Y) ? ((Y - X) / D) + 1 : (Y - X) / D;
+        return (X + D * ((Y - X) / D)) < Y ? (Y - X) / D + 1 : (Y - X) / D;
     }
 }
